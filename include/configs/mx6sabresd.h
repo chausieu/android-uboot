6ccc0aff2982b10b7effd34b4b39f0c74e6ea5fe
wuwenbing <wuwenbing@norco.com.cn>
2017-05-09 22:25:51 +0800

    config UART2_BASE CONFIG_CONSOLE_DEV=ttymxc1

--- include/configs/mx6sabresd.h
+++ include/configs/mx6sabresd.h
@@ -19,9 +19,9 @@
 #endif
 
 #define CONFIG_MACH_TYPE	3980
-#define CONFIG_MXC_UART_BASE	UART1_BASE
-#define CONFIG_CONSOLE_DEV		"ttymxc0"
-#define CONFIG_MMCROOT			"/dev/mmcblk2p2"  /* SDHC3 */
+#define CONFIG_MXC_UART_BASE	UART2_BASE
+#define CONFIG_CONSOLE_DEV		"ttymxc1"
+#define CONFIG_MMCROOT			"/dev/mmcblk0p2"  /* SDHC3 */
 #if defined(CONFIG_MX6QP)
 #define CONFIG_DEFAULT_FDT_FILE	"imx6qp-sabresd.dtb"
 #define PHYS_SDRAM_SIZE		(1u * 1024 * 1024 * 1024)
